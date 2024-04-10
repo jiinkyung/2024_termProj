0892879e80a7b99212530880c0bbb0a4c6a37211
jiinkyung <jinkyung010310@gmail.com>
2024-04-11 00:26:42 +0900

    Init : 프로젝트 세팅

---
+++ src/main/java/com/webApp/termProj/TermProjApplication.java
@@ -0,0 +1,13 @@
+package com.webApp.termProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webApp/termProj/TermProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webApp.termProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
