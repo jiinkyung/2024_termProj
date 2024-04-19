d6af6f3f6fe7785aa27f75466a87ce67c761d72d
jiinkyung <jinkyung010310@gmail.com>
2024-04-20 00:37:42 +0900

    Fix : 불필요한 코드 삭제

--- src/main/java/com/webApp/termProj/repository/BookRepository.java
+++ src/main/java/com/webApp/termProj/repository/BookRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, String> {
-
-    List<BookEntity> findByUserId(String userId);
     List<BookEntity> findByTitle(String title);
 }
